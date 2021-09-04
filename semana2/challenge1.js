70432c658392b14e60041a40f43c9c249166e83f
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2021-09-03 23:43:06 -0600

    faltan 3

--- semana2/challenge1.js
+++ semana2/challenge1.js
@@ -1,5 +1,16 @@
 function twoSums(numbs, target) {
     //Implementación
+    //console.log(numbs);
+    for (let i = 0; i < numbs.length; i++) {
+        for (let j = 0; j < numbs.length; j++) {
+            //console.log(j+".-  " + (numbs[i]+numbs[j]));
+            if(i===j || i<j) continue; // Salto aquellas iteraciones que donde se sumaria el mismo numero y aquellos numero que ya fueron evaluados.
+            if(numbs[i]+numbs[j] === target){
+                //console.log("["+i+", "+j+"] ");
+                return [i,j];
+            }
+        }
+    }
 }
 
 module.exports = twoSums;
--- semana2/challenge2.js
+++ semana2/challenge2.js
@@ -1,5 +1,13 @@
 function divisibleByLeft(n) {
     //Implementación
+    const numbers = n.splice("");
+    console.log(numbers);
+
+    for (let i = 0; i < numbers.length; i++) {;
+
+        const element = numbers[i];
+        
+    }
 }
 
 module.exports = divisibleByLeft;
