a181bb7869f67d00bc3caf4921fbe0190e07e1ed
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2021-09-06 13:11:05 -0600

    cambios

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -1,9 +1,5 @@
 const reverseString = (str) => {
-    //Implementación semana 2
-    console.log("hello semana 2-b");
-    console.log("2");
-    console.log("-");
-    console.log("b");
+    //Implementación
 }
 
 module.exports = {
