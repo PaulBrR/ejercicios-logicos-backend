7ea4818284b499eee12e0e0aebe1205906b3a8c9
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2021-09-15 00:54:43 -0600

    Dejando comentarios

--- semana3/challenge4.js
+++ semana3/challenge4.js
@@ -4,6 +4,8 @@ function toArray(objectToArray) {
     let arrayAux = [];
     let arrayReady = [];
     if(objectToArray){
+        //  for(key in objectToArray ){ console.log(objectToArray[key])};
+        //  for(key in objectToArray ){ console.log(key)};
          for(key in objectToArray ){
              arrayAux.push(key);
              arrayAux.push(objectToArray[key]);
